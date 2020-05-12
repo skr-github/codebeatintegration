13c6c0f1d62021d8fe2e8498ffba382590f6730d
skr-github <65152660+skr-github@users.noreply.github.com>
2020-05-12 12:17:06 +0530

    Create CodacyCodeReviewTest.java

---
+++ CodacyCodeReviewTest.java
@@ -0,0 +1,12 @@
+public class CodacyCodeReviewTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Inside main method()....");
+		System.out.println("Result Message..."+new CodacyCodeReviewTest().getResult());
+	}
+	
+	private String getResult() {
+		return "SUCCESS";
+	}
+
+}
