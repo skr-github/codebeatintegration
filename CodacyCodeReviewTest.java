fc70e1ecbeab0a91d9b215f34e7547dd5f0fd226
skr-github <65152660+skr-github@users.noreply.github.com>
2020-05-12 12:38:10 +0530

    Update CodacyCodeReviewTest.java

--- CodacyCodeReviewTest.java
+++ CodacyCodeReviewTest.java
@@ -2,7 +2,7 @@ public class CodacyCodeReviewTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Inside main method()....");
-		System.out.println("Result Message..."+new CodacyCodeReviewTest().getResult());
+		
 	}
 	
 	private String getResult() {
